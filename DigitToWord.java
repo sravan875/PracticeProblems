5964b0872e74591bafbdc607e2577d1f67cfc20c
sravan875 <sravankumarkona875@gmail.com>
2026-04-16 10:51:46 +0530

    digit to word

--- DigitToWord.java
+++ DigitToWord.java
@@ -6,17 +6,16 @@ public class DigitToWord {
         Scanner sc = new Scanner(System.in);
         int n = sc.nextInt();
 
-        if (n == 0) System.out.println("Zero");
-        else if (n == 1) System.out.println("One");
-        else if (n == 2) System.out.println("Two");
-        else if (n == 3) System.out.println("Three");
-        else if (n == 4) System.out.println("Four");
-        else if (n == 5) System.out.println("Five");
-        else if (n == 6) System.out.println("Six");
-        else if (n == 7) System.out.println("Seven");
-        else if (n == 8) System.out.println("Eight");
-        else if (n == 9) System.out.println("Nine");
-        else System.out.println("Invalid");
+        String[] words = {
+            "Zero","One","Two","Three","Four",
+            "Five","Six","Seven","Eight","Nine"
+        };
+
+        if (n >= 0 && n <= 9) {
+            System.out.println(words[n]);
+        } else {
+            System.out.println("Invalid");
+        }
 
         sc.close();
     }
