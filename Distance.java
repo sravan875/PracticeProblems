affd0980914fc844e3f91869e1f47006561c3bcc
sravan875 <sravankumarkona875@gmail.com>
2026-04-16 15:46:13 +0530

    Distance

---
+++ Distance.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Distance {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int x = sc.nextInt();
+        int y = sc.nextInt();
+
+        double distance = Math.sqrt(x*x + y*y);
+
+        System.out.println("Distance = " + distance);
+    }
+}
